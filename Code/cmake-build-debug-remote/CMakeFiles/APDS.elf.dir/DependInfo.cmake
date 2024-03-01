c389ae92e2f4929ac0b4529353f22531918c9972
Marcel <62188390+marcelkoniecz@users.noreply.github.com>
2024-03-01 13:26:57 +0100

    added-cmake-build-debug-remote

---
+++ Code/cmake-build-debug-remote/CMakeFiles/APDS.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Startup/startup_stm32f030k6tx.s" "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/cmake-build-debug-remote/CMakeFiles/APDS.elf.dir/Core/Startup/startup_stm32f030k6tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F030x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Inc"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Inc"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/adc.c" "CMakeFiles/APDS.elf.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/adc.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/gpio.c" "CMakeFiles/APDS.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/main.c" "CMakeFiles/APDS.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/stm32f0xx_hal_msp.c" "CMakeFiles/APDS.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/stm32f0xx_it.c" "CMakeFiles/APDS.elf.dir/Core/Src/stm32f0xx_it.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/stm32f0xx_it.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/syscalls.c" "CMakeFiles/APDS.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/sysmem.c" "CMakeFiles/APDS.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Core/Src/system_stm32f0xx.c" "CMakeFiles/APDS.elf.dir/Core/Src/system_stm32f0xx.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Core/Src/system_stm32f0xx.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_adc_ex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj.d"
+  "D:/Uczelnia/Semestr IX/Analog Peripherals of Digital System/project2/APDS/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/APDS.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
