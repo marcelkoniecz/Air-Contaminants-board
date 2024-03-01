de3d904603e80c48d0883cc689065bc8d84a6bdf
Marcel <62188390+marcelkoniecz@users.noreply.github.com>
2024-03-01 13:26:08 +0100

    Added cmake-build-debug

---
+++ Code/cmake-build-debug/CMakeFiles/APDS.elf.dir/Core/Src/syscalls.c.obj.d
@@ -0,0 +1,45 @@
+CMakeFiles/APDS.elf.dir/Core/Src/syscalls.c.obj: \
+ D:\Uczelnia\Semestr\ IX\Analog\ Peripherals\ of\ Digital\ System\project2\APDS\Core\Src\syscalls.c \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\stat.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\_ansi.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\newlib.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\_newlib_version.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\config.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\ieeefp.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\features.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\time.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\_ansi.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\cdefs.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\programy\eabi\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\reent.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_types.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\_types.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\lock.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\time.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\types.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_stdint.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\endian.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\_endian.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\select.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_sigset.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_timeval.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\timespec.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_timespec.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_pthreadtypes.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\sched.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\types.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\_locale.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\stdlib.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\stdlib.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\alloca.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\errno.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\errno.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\stdio.h \
+ d:\programy\eabi\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stdarg.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\stdio.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\signal.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\signal.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\time.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\machine\_time.h \
+ d:\programy\eabi\102021~1.10\arm-none-eabi\include\sys\times.h
